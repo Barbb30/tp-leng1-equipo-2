0c75957777388ed3680f7a2e889b65a56ed5ad7b
Cristaldesebas <cristaldesebastian97@gmail.com>
2026-07-02 16:43:16 -0300

    Update script.js

--- css/script.js
+++ css/script.js
@@ -1,17 +1,1 @@
-const boton = document.getElementById("modo");
 
-boton.addEventListener("click", function(){
-
-    document.body.classList.toggle("dark-mode");
-
-    if(document.body.classList.contains("dark-mode")){
-
-        boton.innerHTML="☀️ Modo claro";
-
-    }else{
-
-        boton.innerHTML="🌙 Modo oscuro";
-
-    }
-
-});
